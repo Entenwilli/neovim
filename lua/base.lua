3047e6a0e00574031fe4192f6d77acc1a7b4c1d4
Felix Schwickerath <felix@fschwickerath.de>
2023-04-14 10:42:34 +0200

    Add play/pause button config

--- lua/base.lua
+++ lua/base.lua
@@ -1,5 +1,8 @@
 vim.cmd('autocmd!')
 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 vim.scriptencoding = 'utf-8'
 vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
@@ -15,9 +18,9 @@ vim.opt.ignorecase = true
 vim.opt.smarttab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
-vim.opt.ai = true -- Auto indent
-vim.opt.si = true -- Smart indent
-vim.opt.wrap = false -- No wrapping lines
+vim.opt.ai = true            -- Auto indent
+vim.opt.si = true            -- Smart indent
+vim.opt.wrap = false         -- No wrapping lines
 vim.opt.backspace = 'start,eol,indent'
 vim.opt.path:append { '**' } -- Enable subdirectory search
 
