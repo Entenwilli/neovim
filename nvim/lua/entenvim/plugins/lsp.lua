3a7be2f2eb521845de1a16f9d2a4024348112f4e
Felix Schwickerath <felix@fschwickerath.de>
2024-07-27 10:34:04 +0200

    feat(lsp): disable inlay hints

--- nvim/lua/entenvim/plugins/lsp.lua
+++ nvim/lua/entenvim/plugins/lsp.lua
@@ -36,7 +36,7 @@ return {
 			-- Be aware that you also will need to properly configure your LSP server to
 			-- provide the inlay hints.
 			inlay_hints = {
-				enabled = true,
+				enabled = false,
 			},
 			-- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
 			-- Be aware that you also will need to properly configure your LSP server to
