68de754fa1de5f8b4557ae36f778f5a7cdac3ece
Felix Schwickerath <felix@fschwickerath.de>
2024-11-12 20:38:03 +0100

    chore(nix): update packages

--- nvim/lua/entenvim/plugins/lsp.lua
+++ nvim/lua/entenvim/plugins/lsp.lua
@@ -186,7 +186,7 @@ return {
 						},
 					},
 				},
-				tsserver = {
+				ts_ls = {
 					settings = {
 						---@diagnostic disable-next-line: missing-fields
 						typescript = {
@@ -242,15 +242,7 @@ return {
 			-- you can do any additional lsp server setup here
 			-- return true if you don't want this server to be setup with lspconfig
 			---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-			setup = {
-				-- example to setup with typescript.nvim
-				-- tsserver = function(_, opts)
-				--   require("typescript").setup({ server = opts })
-				--   return true
-				-- end,
-				-- Specify * to use this function as a fallback for any server
-				-- ["*"] = function(server, opts) end,
-			},
+			setup = {},
 		},
 		---@param opts PluginLspOpts
 		config = function(_, opts)
