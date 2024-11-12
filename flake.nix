24396a25ceba4a58ed1bca27ee0d82e6b8e9f4b3
Felix Schwickerath <felix@fschwickerath.de>
2024-11-12 22:29:43 +0100

    feat(nix): update to module based setup

--- flake.nix
+++ flake.nix
@@ -4,51 +4,40 @@
   inputs = {
     # NixOS packages
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    # Flake utils
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = inputs @ {
     self,
     nixpkgs,
-    flake-parts,
     ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux"];
-      flake = {
-        lib = import ./lib {inherit inputs;};
-        homeManagerModules.default = import ./lib/homemanager.nix self;
-        overlays.default = final: prev: {
-          neovim = self.packages.${prev.system}.neovim;
+  }: let
+    inherit (nixpkgs) lib;
+    withSystem = f:
+      lib.fold lib.recursiveUpdate {} (
+        map f [
+          "x86_64-linux"
+        ]
+      );
+  in
+    withSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeManagerModules = {
+        neovim = import "${self}/lib/default.nix" {
+          inherit self system;
+          isNixOSModule = false;
         };
+        default = self.homeManagerModules.neovim;
       };
-      perSystem = {
-        config,
-        self',
-        inputs,
-        pkgs,
-        system,
-        ...
-      }: let
-        inherit (pkgs) alejandra;
-      in {
-        packages = rec {
-          neovim = self.lib.mkNeovim {inherit system;};
-          #neovim = nixpkgs.legacyPackages.${system}.callPackage ./lib/neovim {
-          #  neovim = pkgs.neovim;
-          #};
-          default = neovim;
-          config = neovim.passthru.config;
+
+      nixosModules = {
+        neovim = import "${self}/lib/default.nix" {
+          inherit self system;
+          isNixOSModule = true;
         };
-        formatter = alejandra;
-        apps = {
-          nvim = {
-            program = "${self.packages.${system}.packages.neovim}/bin/nvim";
-            type = "app";
-          };
-        };
+        default = self.nixosModules.neovim;
       };
-    };
+
+      formatter.${system} = pkgs.alejandra;
+    });
 }
