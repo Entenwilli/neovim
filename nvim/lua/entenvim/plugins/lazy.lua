8a7463bad63dfdc797ce3eaa6ac32571f46d3ab5
Felix Schwickerath <felix@fschwickerath.de>
2024-12-21 12:52:24 +0100

    feat(projects): add projects plugin

--- nvim/lua/entenvim/plugins/lazy.lua
+++ nvim/lua/entenvim/plugins/lazy.lua
@@ -30,6 +30,7 @@ return {
           keys = {
             { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
             { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            { icon = " ", key = "p", desc = "Projects", action = ":Telescope projects"},
             { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
             { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
             { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
--- nvim/lua/entenvim/plugins/ui.lua
+++ nvim/lua/entenvim/plugins/ui.lua
@@ -75,4 +75,26 @@ return {
 			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
 		},
 	},
+	{
+		"ahmedkhalf/project.nvim",
+		opts = {
+			manual_mode = true,
+		},
+		event = "VeryLazy",
+		config = function(_, opts)
+			require("project_nvim").setup(opts)
+			local history = require("project_nvim.utils.history")
+			history.delete_project = function(project)
+				for k, v in pairs(history.recent_projects) do
+					if v == project.value then
+						history.recent_projects[k] = nil
+						return
+					end
+				end
+			end
+			EntenVim.on_load("telescope.nvim", function()
+				require("telescope").load_extension("projects")
+			end)
+		end,
+	},
 }
