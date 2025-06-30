d7e12754b466ec962785c97bf740c0ea5266ae2c
Felix Schwickerath <felix@fschwickerath.de>
2025-06-30 22:09:24 +0200

    feat(neovide): update neovide options

--- nvim/lua/entenvim/plugins/colorscheme.lua
+++ nvim/lua/entenvim/plugins/colorscheme.lua
@@ -2,6 +2,9 @@ return {
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
+		opts = {
+			term_colors = true,
+		},
 		priority = 1000,
 		init = function()
 			vim.cmd([[colorscheme catppuccin-mocha]])
--- nvim/lua/entenvim/user/options.lua
+++ nvim/lua/entenvim/user/options.lua
@@ -57,4 +57,22 @@ vim.opt.background = "dark"
 -- Neovide settings
 if vim.g.neovide then
 	vim.opt.guifont = "FiraCode Nerd Font:h14" -- the font used in graphical neovim applications
+	vim.g.neovide_theme = "auto"
+	vim.g.neovide_refresh_rate = 144
+	vim.g.terminal_color_0 = "#45475a"
+	vim.g.terminal_color_1 = "#f38ba8"
+	vim.g.terminal_color_2 = "#a6e3a1"
+	vim.g.terminal_color_3 = "#f9e2af"
+	vim.g.terminal_color_4 = "#89b4fa"
+	vim.g.terminal_color_5 = "#f5c2e7"
+	vim.g.terminal_color_6 = "#94e2d5"
+	vim.g.terminal_color_7 = "#bac2de"
+	vim.g.terminal_color_8 = "#585b70"
+	vim.g.terminal_color_9 = "#f38ba8"
+	vim.g.terminal_color_10 = "#a6e3a1"
+	vim.g.terminal_color_11 = "#f9e2af"
+	vim.g.terminal_color_12 = "#89b4fa"
+	vim.g.terminal_color_13 = "#f5c2e7"
+	vim.g.terminal_color_14 = "#94e2d5"
+	vim.g.terminal_color_15 = "#a6adc8"
 end
