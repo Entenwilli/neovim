81222fa8b0b9aba8d184655b082969a46f89bdab
Felix Schwickerath <felix@fschwickerath.de>
2025-07-05 11:16:41 +0200

    fix(projects): do not restore project when entering vim without directory

--- nvim/lua/entenvim/plugins/lazy.lua
+++ nvim/lua/entenvim/plugins/lazy.lua
@@ -30,11 +30,11 @@ return {
           keys = {
             { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
             { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-            { icon = " ", key = "p", desc = "Projects", action = ":Telescope projects"},
+            { icon = " ", key = "p", desc = "Projects", action = ":NeovimProjectDiscover"},
             { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
             { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
             { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session", action = ":NeovimProjectLoadRecent" },
             { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
             { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
             { icon = " ", key = "q", desc = "Quit", action = ":qa" },
--- nvim/lua/entenvim/plugins/ui.lua
+++ nvim/lua/entenvim/plugins/ui.lua
@@ -58,7 +58,14 @@ return {
 			},
 			picker = {
 				type = "telescope",
+				preview = {
+					enabled = true,
+					git_status = true,
+					git_fetch = true,
+					show_hidden = true,
+				},
 			},
+			last_session_on_startup = false,
 		},
 		init = function()
 			vim.opt.sessionoptions:append("globals")
