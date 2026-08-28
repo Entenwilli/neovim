78269d784a18d5c7fd63b6804f6424dc9de771f1
Felix Schwickerath <felix@fschwickerath.de>
2026-08-28 08:21:47 +0200

    feat(neotree): update neotree config

--- modules/editor/neotree.nix
+++ modules/editor/neotree.nix
@@ -1,6 +1,61 @@
 {
   plugins.neo-tree = {
     enable = true;
+    settings = {
+      close_if_last_window = true;
+      enable_git_status = true;
+      enable_diagnostics = true;
+      sources = [
+        "filesystem"
+        "buffers"
+        "git_status"
+      ];
+      filesystem = {
+        filtered_items = {
+          visible = false;
+          hide_dotfiles = true;
+          hide_gitignored = true;
+        };
+      };
+      source_selector = {
+        winbar = true;
+        statusline = true;
+      };
+      default_component_configs = {
+        indent = {
+          indent_size = 2;
+          padding = 1;
+          with_markers = true;
+          indent_marker = "│";
+          last_indent_marker = "└";
+          highlight = "NeoTreeIndentMarker";
+          with_expanders = true;
+          expander_collapsed = "";
+          expander_expanded = "";
+          expander_highlight = "NeoTreeExpander";
+        };
+        modified = {
+          symbol = "[+]";
+          highlight = "NeoTreeModified";
+        };
+        name = {
+          trailing_slash = false;
+          use_git_status_colors = true;
+          highlight = "NeoTreeFileName";
+        };
+        git_status = {
+          symbols = {
+            added = "✚";
+            modified = "";
+            untracked = "";
+            ignored = "";
+            unstaged = "U";
+            staged = "";
+            conflict = "";
+          };
+        };
+      };
+    };
   };
   keymaps = [
     {
